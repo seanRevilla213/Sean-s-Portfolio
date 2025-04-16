290ad18f94d744cfdbcd0f5b0ac8b67af791cd7d
seanRevilla213 <161412203+seanRevilla213@users.noreply.github.com>
2025-04-16 21:40:20 +0800

    Add files via upload

---
+++ Sean's PortFolio/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["BackEnd Developer . . ." , "Programmer . . ." , "Web/Soft Developer . . ."],
+    typeSpeed: 70,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
